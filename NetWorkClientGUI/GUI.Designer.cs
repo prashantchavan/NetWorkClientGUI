98a8a5513d961c67cdf1434f2538d8b6a40319c3
Prashant Chavan <prashant.j.chavan@gmail.com>
2015-12-03 07:45:38 -0800

    UI PHASE1 COMPLETE

---
+++ NetWorkClientGUI/GUI.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace NetWorkClientGUI
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtServerAddress = new System.Windows.Forms.TextBox();
+            this.txtPortAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCommunication = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.History = new System.Windows.Forms.GroupBox();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.History.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPortAddress);
+            this.groupBox1.Controls.Add(this.txtServerAddress);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server Details";
+            // 
+            // txtServerAddress
+            // 
+            this.txtServerAddress.Location = new System.Drawing.Point(6, 44);
+            this.txtServerAddress.Name = "txtServerAddress";
+            this.txtServerAddress.Size = new System.Drawing.Size(228, 20);
+            this.txtServerAddress.TabIndex = 0;
+            // 
+            // txtPortAddress
+            // 
+            this.txtPortAddress.Location = new System.Drawing.Point(248, 44);
+            this.txtPortAddress.Name = "txtPortAddress";
+            this.txtPortAddress.Size = new System.Drawing.Size(113, 20);
+            this.txtPortAddress.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port No";
+            // 
+            // txtCommunication
+            // 
+            this.txtCommunication.Location = new System.Drawing.Point(6, 23);
+            this.txtCommunication.Multiline = true;
+            this.txtCommunication.Name = "txtCommunication";
+            this.txtCommunication.ReadOnly = true;
+            this.txtCommunication.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCommunication.Size = new System.Drawing.Size(355, 199);
+            this.txtCommunication.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtCommunication);
+            this.groupBox2.Location = new System.Drawing.Point(13, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(370, 233);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Messages";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtMessage);
+            this.groupBox3.Controls.Add(this.btnPop);
+            this.groupBox3.Controls.Add(this.btnPush);
+            this.groupBox3.Location = new System.Drawing.Point(13, 332);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(370, 55);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(6, 20);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(75, 23);
+            this.btnPush.TabIndex = 0;
+            this.btnPush.Text = "Push";
+            this.btnPush.UseVisualStyleBackColor = true;
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(285, 21);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(75, 23);
+            this.btnPop.TabIndex = 1;
+            this.btnPop.Text = "Pop";
+            this.btnPop.UseVisualStyleBackColor = true;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(88, 22);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(191, 20);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // History
+            // 
+            this.History.Controls.Add(this.txtHistory);
+            this.History.Location = new System.Drawing.Point(13, 394);
+            this.History.Name = "History";
+            this.History.Size = new System.Drawing.Size(370, 100);
+            this.History.TabIndex = 4;
+            this.History.TabStop = false;
+            this.History.Text = "History";
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(6, 20);
+            this.txtHistory.Multiline = true;
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHistory.Size = new System.Drawing.Size(354, 74);
+            this.txtHistory.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 500);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Network Client";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.History.ResumeLayout(false);
+            this.History.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtServerAddress;
+        private System.Windows.Forms.TextBox txtPortAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCommunication;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.GroupBox History;
+        private System.Windows.Forms.TextBox txtHistory;
+    }
+}
+
